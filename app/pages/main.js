af1e1a9ccd51a92201156fbce7d18b5b2af0cec7
Linux <gem-stone@localhost.localdomain>
2023-07-16 20:46:27 +0100

    last

--- app/pages/main.js
+++ app/pages/main.js
@@ -8,7 +8,7 @@ function Main() {
     <section className='text-gray-400 flex text-3xl justify-evenly '>
         {categories.map((item)=><div key={Math.random()} className=' hover:bg-orange-400 flex  flex-col justify-center items-center p-2 rounded-md hover:text-white'>{item.icon}<p className='text-xs' >{item.name}</p></div>)}
     </section>
-    <div className='font-poppins justify-center p-0 flex flex-wrap' > {data.map((product)=><ProductCard key={name} image={product.image} price={product.price} name={product.name} />)}</div>
+    <div className='font-poppins justify-center p-0 flex flex-wrap' > {data.map((product)=><ProductCard key={product.name} image={product.image} price={product.price} name={product.name} />)}</div>
     </main>
   )
 }
