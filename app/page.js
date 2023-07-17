1bd894ec31e31d3cd2c17cc3b049f92434a4b2e6
Linux <gem-stone@localhost.localdomain>
2023-07-17 15:17:51 +0100

    hi

--- app/page.js
+++ app/page.js
@@ -1,30 +1,29 @@
-"use client"
-import Image from 'next/image'
-import { Poppins } from 'next/font/google'
-import ProductCard from './pages/components/productCard'
-import Main from './pages/main'
-import { HiHeart, HiHome, HiMenuAlt1, HiSearch, HiUser } from 'react-icons/hi'
-import { useRouter } from 'next/navigation'
+"use client";
+import Image from "next/image";
+import { Poppins } from "next/font/google";
+import ProductCard from "./pages/components/productCard";
+import Main from "./pages/main";
+import { HiHeart, HiHome, HiMenuAlt1, HiSearch, HiUser } from "react-icons/hi";
+import { useRouter } from "next/navigation";
 
-import Header from './pages/components/header'
+import Header from "./pages/components/header";
 export default function Home() {
-  const router = useRouter()
+  const router = useRouter();
   return (
-    <main className='w-[100vw] h-[1000vh]'>
-      <div className='bg-orange-900  fixed w-full'>
+    <main className="w-[100vw] h-[1000vh]">
+      <div className="bg-orange-900 z-20 fixed w-full">
         <Header />
       </div>
-    <section className='relative'>
+      <section className="relative">
+        <Main />
+      </section>
 
-        <Main />  
-    </section>
-
-    <section className='fixed flex items-center w-[100vw] justify-evenly px-2 h-[10vh] bottom-0 left-0 right-0 bg-slate-800 bg-opacity-70 rounded-t-lg text-white' > 
-      <HiHome onClick={()=>router.push("@/app/pages/login")} />
-      <HiSearch />
-      <HiHeart />
-      <HiUser />
-    </section>
+      <section className="fixed flex items-center w-[100vw] justify-evenly px-2 h-[10vh] bottom-0 left-0 right-0 bg-slate-800 bg-opacity-70 rounded-t-lg text-white">
+        <HiHome onClick={() => router.push("@/app/pages/login")} />
+        <HiSearch />
+        <HiHeart />
+        <HiUser />
+      </section>
     </main>
-  )
+  );
 }
